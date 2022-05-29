78585135348111a40ef6f8b42a7159ce3fff884b
dgoncalvesramos <david.goncalves.ramos@gmail.com>
2022-05-29 12:33:24 +0200

    Add some kata

---
+++ maximun_subarray_sum.py
@@ -0,0 +1,14 @@
+#https://www.codewars.com/kata/54521e9ec8e60bc4de000d6c/
+
+def max_sequence(arr):
+    
+    if (len(arr)==0):
+        return 0
+    
+    max_so_far =arr[0]
+    curr_max = arr[0]
+    for i in range(len(arr)):
+        curr_max = max(arr[i], curr_max + arr[i])
+        max_so_far = max(max_so_far,curr_max)
+    return max_so_far
+          
