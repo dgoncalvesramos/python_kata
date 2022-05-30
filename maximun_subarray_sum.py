5bc808f5d99493fb2d383f19cc086976b0c4a267
dgoncalvesramos <david.goncalves.ramos@gmail.com>
2022-05-30 14:05:40 +0200

    Correct maximun subarray sum kata

--- maximun_subarray_sum.py
+++ maximun_subarray_sum.py
@@ -7,8 +7,11 @@ def max_sequence(arr):
     
     max_so_far =arr[0]
     curr_max = arr[0]
-    for i in range(len(arr)):
+    for i in range(1,len(arr)):
         curr_max = max(arr[i], curr_max + arr[i])
         max_so_far = max(max_so_far,curr_max)
+    if(max_so_far<0) :
+        return 0
     return max_so_far
+                   
           
